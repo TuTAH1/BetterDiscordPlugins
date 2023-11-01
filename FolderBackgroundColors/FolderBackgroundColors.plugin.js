95bc319e6143f7e17474653eff39ac79b3b2b66f
Титан <fignin@ya.ru>
2023-11-01 22:49:11 +0500

    Update FolderBackgroundColors.plugin.js
    Replaced SetInterval with MutationObserver; fixed classes for Discord update

--- FolderBackgroundColors/FolderBackgroundColors.plugin.js
+++ FolderBackgroundColors/FolderBackgroundColors.plugin.js
@@ -1,7 +1,7 @@
 /**!
  * @name FolderBackgroundColors
  * @description Makes the background colors of the folders the same colors as folders, instead of standart one color for all folder backgrounds
- * @version 1.0.0
+ * @version 1.1.0
  * @author Титан
  * @authorId https://discordapp.com/users/282775588257792005/
  * @authorLink http://steamcommunity.com/id/TuTAH_1/
@@ -12,7 +12,7 @@ const config = {
 	info: {
 		name: "FolderBackgroundColors",
 		description: "Makes the background colors of the folders the same colors as folders, instead of standart one color for all folder backgrounds",
-		version: "1.0.0",
+		version: "1.1.0",
 		author: "Титан",
 		updateUrl: "https://raw.githubusercontent.com/TuTAH1/BetterDiscordPlugins/main/FolderBackgroundColors/FolderBackgroundColors.js"
 	}
@@ -26,21 +26,65 @@ module.exports = class FolderBackgroundColors {
 	getAuthor() { return config.info.author; }
 
 	async start() {
-		console.log("starting FolderBackgroundColors...")
+		let logging = true;
+
+		function log(message) {
+			if (logging) console.log(message);
+		}
+
+		log("starting FolderBackgroundColors...")
 		const FoldericonSelector = "div[class*=\"expandedFolderIconWrapper\"] > svg > path";
-		const FoldersSelector = "span[class*=\"expandedFolderBackground\"]"
-		let Folders = await waitForElm(FoldersSelector); // Несколько бесполезно, поскольку всё равно сбивается при старте. Надо сделать выполнение main при каждой "мутации"
+		const FoldersSelector = "span[class*=\"expandedFolderBackground\"]";
 		let CssString;
 		let FolderNumber = 0;
-		let MainInterval;
+		let observerFoundFolders = false; // костыль for this ugly disguasting MutationObserver
+
+		function handleMutation(mutationsList, observer) {
+			for (let mutation of mutationsList) {
+				if (mutation.type === 'childList' && (mutation.addedNodes.length > 0 || mutation.removedNodes.length > 0)) {
+					let Folders = document.querySelectorAll(FoldersSelector);
+					if (Folders !== null) {
+						if (!observerFoundFolders) { //set observer to observe only folders
+							observer.disconnect();
+							observer = new MutationObserver(handleMutation);
+							observer.observe(Folders[0].parentElement.parentElement, { childList: true, subtree: true })
+						}
+						main(Folders); // plugin work
+					} else if (observerFoundFolders) { //set observer to observe all body
+						observer.disconnect();
+						observer = new MutationObserver(handleMutation);
+						observer.observe(document.body, { childList: true, subtree: true })
+					}
+				}
+			}
+		}
+
+		const observer = new MutationObserver(handleMutation);
+		observer.observe(document.body, { childList: true, subtree: true });
+
+
+		//! MAIN FUNCTION
+		function main(Folders) {
+			let OldCss = CssString;
+
+			for (let folder of Folders) {
+				colorize(folder)
+			}
+
+			if (OldCss != CssString) {
+				BdApi.injectCSS(config.info.name, CssString);
+				log("css string:")
+				log(CssString)
+			}
+		}
 
 		function getStyle(element) {
 			if (element === null) {
-				console.log("FBC GetStyle error: elements is null")
+				log("FBC GetStyle error: elements is null")
 				return null;
 			}
-			console.log("Getting style of");
-			console.log(element);
+			log("Getting style of");
+			log(element);
 			return getComputedStyle(element, null);
 		}
 
@@ -62,10 +106,10 @@ module.exports = class FolderBackgroundColors {
 
 		function getFolderBackground(folder) {
 			if (folder.className.indexOf("collapsed") >= 0) {
-				return getStyle(folder.parentElement.querySelector("div[class*=\"folderIconWrapper-\"]")).backgroundColor;
+				return getStyle(folder.parentElement.querySelector("div[class*=\"folderIconWrapper_\"]")).backgroundColor;
 			} else {
-				let folderIcon = folder.nextSibling.querySelector(FoldericonSelector)
-				let folderColor = getStyle(folderIcon).fill;
+				let folderIcon = folder.nextSibling.querySelector(FoldericonSelector) //svg
+				let folderColor = getStyle(folderIcon).color;
 				if (folderColor === null) return null;
 
 				return changeColorOpacity(folderColor, 0.4);
@@ -99,41 +143,24 @@ module.exports = class FolderBackgroundColors {
 			});
 		}
 
-		//! MAIN FUNCTION
-		function main() {
-			Folders = document.querySelectorAll(FoldersSelector);
-			if (Folders === null) {
-				//console.log("Folders: Почему ты меня не подождал? Observer: Да");
-				return
-			}
-			let OldCss = CssString;
-
-			for (let folder of Folders) {
-				colorize(folder)
-			}
-
-			if (OldCss != CssString) {
-				BdApi.injectCSS(config.info.name, CssString);
-				console.log("css string:")
-				console.log(CssString)
-			}
-		}
-
-		MainInterval = setInterval(main,5000); //It's better to make it with observer, but I have not time yet to learn it.
-
 	} // Required function. Called when the plugin is activated (including after reloads)
 
 	stop() {
-		console.log("stopping FolderBackgroundColors...")
+		let logging = true;
+
+		function log(message) {
+			if (logging) console.log(message);
+		}
+
+		log("stopping FolderBackgroundColors...")
 		const Folders = document.querySelectorAll("span[class*=\"expandedFolderBackground\"]");
 		if (Folders == null) {
-			console.log("FBC error: no folders found"); return
+			log("FBC error: no folders found"); return
 		}
 		for(let folder of Folders) {
 			folder.removeAttribute("fbc_id");
 		}
 		BdApi.clearCSS(config.info.name);
-		clearInterval(MainInterval);
 	} // Required function. Called when the plugin is deactivated
 
 	observer(changes) {} // Optional function. Observer for the `document`. Better documentation than I can provide is found here: <https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver>
