2b170b579948c76559e844df412cfd46f23fb1f5
Титан <fignin@ya.ru>
2024-01-04 22:37:19 +0500

    Update FolderBackgroundColors.plugin.js
    small code cleanup

--- FolderBackgroundColors/FolderBackgroundColors.plugin.js
+++ FolderBackgroundColors/FolderBackgroundColors.plugin.js
@@ -1,7 +1,7 @@
 /**!
  * @name FolderBackgroundColors
  * @description Makes the background colors of the folders the same colors as folders, instead of standart one color for all folder backgrounds
- * @version 1.1.3
+ * @version 1.1.3.2
  * @author Титан
  * @authorId https://discordapp.com/users/282775588257792005/
  * @authorLink http://steamcommunity.com/id/TuTAH_1/
@@ -12,7 +12,7 @@ const config = {
 	info: {
 		name: "FolderBackgroundColors",
 		description: "Makes the background colors of the folders the same colors as folders, instead of standart one color for all folder backgrounds",
-		version: "1.1.3",
+		version: "1.1.3.2",
 		author: "Титан",
 		authorId: "https://discordapp.com/users/282775588257792005/",
 		authorLink: "http://steamcommunity.com/id/TuTAH_1/",
@@ -148,186 +148,6 @@ module.exports = class FolderBackgroundColors {
 			let colors = color.replaceAll(/[^\d| ]/g, '').split(" ", 3);
 			return "rgba(" + colors + ", " + newOpacity + ')'
 		}
-
-		function colorizeClosed(folder) {
-			if (folder.className.indexOf("colored") >= 0) return;
-			folder.className += " colored";
-		}
-	
-
-		// Ext funcs
-		function waitForElm(selector) {
-			return new Promise(resolve => {
-				if (document.querySelectorAll(selector)) {
-					return resolve(document.querySelectorAll(selector));
-				}
-
-				const observer = new MutationObserver(mutations => {
-					if (document.querySelectorAll(selector)) {
-						resolve(document.querySelectorAll(selector));
-						observer.disconnect();
-					}
-				});
-
-				observer.observe(document.body, {
-					childList: true,
-					subtree: true
-				});
-			});
-		}
-
-
-		//! Part of Titanium.js (my lib)
-
-		if (!String.prototype.isNullOrEmpty) {
-			String.prototype.isNullOrEmpty = function () {
-				return (this === null || this === undefined || this === '');
-			};
-		}
-
-		function swap(array, aIndex, bIndex) {
-			var temp = array[aIndex];
-			array[aIndex] = array[bIndex];
-			array[bIndex] = temp;
-		}
-
-		if (!String.prototype.Tslice) {
-			String.prototype.Tslice = function (Start, End, AlwaysReturnString = false, LastStart = false, LastEnd = true, IncludeStart = false, IncludeEnd = false) {
-				if (this.isNullOrEmpty())
-					if (AlwaysReturnString)
-						return null;
-					else
-						throw new Error("String is null or empty");
-
-				let start;
-				let end;
-				let BasicSlice = (typeof Start) === "number" && (typeof End) === "number";
-				let result = this;
-
-				switch (typeof Start) {
-					case "number":
-						start = Start;
-						if (start < 0) start = this.length + start; //: count from end if negative
-						if (start < 0 || start >= this.length)
-							if (AlwaysReturnString)
-								start = 0;
-							else
-								return null;
-						break;
-					case "string":
-						start = LastStart ? this.lastIndexOf(Start) : this.indexOf(Start);
-						if (start < 0) start = 0;
-						if (IncludeStart) start += Start.length;
-						break;
-					case "object":
-						if (Start instanceof RegExp) {
-							let match = LastStart ? Start.exec(this)[array.length - 1] : Start.exec(this)[0];
-							start = match.index >= 0 ?
-								(match.index + (IncludeStart ? 0 : match[0].length)) : 0;
-						} else if (Start instanceof Array && Start.every(x => typeof x == "function")) {
-							start = Start.length > 0 ?
-								this.indexOfT({Start, IndexOfEnd: !IncludeStart, RightDirection: !LastStart}) : 0;
-							if (start < 0) start = 0;
-						} else
-							throw new Error("Type of Start is not supported");
-						break;
-					default:
-						throw new Error("Type of Start is not supported");
-				}
-
-				if (!BasicSlice)
-					result = this.slice(start);
-
-
-				switch (typeof End) {
-					case "number":
-						end = End;
-						if (end < 0) end = this.length + end; //: count from end if negative
-						if (BasicSlice && start > end) Swap(start, end);
-						if (end > this.length) end = this.length;
-						break;
-					case "string":
-						end = (LastEnd ? this.lastIndexOf(End) : this.indexOf(End));
-						if (end < 0) end = this.length;
-						if (IncludeEnd) end += End.length;
-						break;
-					case "object":
-						if (End instanceof RegExp) {
-							let match = LastEnd ? End.exec(this).last() : End.exec(this);
-							end = match.index >= 0 ?
-								(match.index + (LastEnd ? 0 : match[0].length)) : 0;
-						} else if (End instanceof Array && End.every(x => typeof x == "function")) {
-							end = End.length > 0 ?
-								this.indexOfT({End, IndexOfEnd: IncludeEnd, RightDirection: !LastEnd}) : 0;
-							if (end < 0)
-								if (AlwaysReturnString)
-									end = this.length - 1;
-								else
-									return null;
-						} else throw new Error("Type of End is not supported");
-						break;
-					default:
-						throw new Error("Type of End is not supported");
-				}
-
-				return BasicSlice ?
-					result.slice(start, (end - start)) :
-					result.slice(0, end);
-			}
-		}
-
-		if (!String.prototype.indexOfT) {
-			String.prototype.indexOfT = function (Conditions, Start = 0, End = Infinity, RightDirection = true, IndexOfEnd = false) {
-				if (End === Infinity) End = this.length - 1;
-				if (Start < 0) Start = this.length + Start;
-				if (Start < 0) throw new Error(`incorrect negative Start (${Start - this.length}). |Start| should be ≤ this.length (${this.length})`);
-				if (End < 0) End = this.length + End;
-				if (End < 0) throw new Error(`incorrect negative End (${End - this.length}). |End| should be ≤ this.length (${this.length})`);
-
-				if (End === Start) return -2;
-
-				if (RightDirection && End < Start ||
-					!RightDirection && End > Start)
-					swap(Start, End);
-
-				let defaultCurMatchPos = RightDirection ? 0 : Conditions.length - 1;
-				let curCondition = defaultCurMatchPos;
-				let Result = -1;
-
-				if (RightDirection)
-					for (let i = Start; i < End; i++) {
-						if (Conditions[curCondition](this[i])) {
-							curCondition++;
-							if (curCondition !== Conditions.length) continue;
-							Result = i;
-							curCondition = defaultCurMatchPos;
-							//if(!LastOccuarance)
-							break;
-						} else {
-							i -= curCondition;
-							curCondition = defaultCurMatchPos;
-						}
-					}
-				else
-					for (let i = Start; i >= End; i--) {
-						if (Conditions[curCondition](this[i])) {
-							curCondition--;
-							if (curCondition !== 0) continue;
-							Result = i;
-							curCondition = defaultCurMatchPos;
-							//if(!LastOccuarance)
-							break;
-						} else {
-							i += ((Conditions.length - 1) - curCondition);
-							curCondition = defaultCurMatchPos;
-						}
-					}
-
-				return (Result === -1 || IndexOfEnd ^ !RightDirection) ?
-					Result : (Result - Conditions.length) + 1;
-			}
-		}
-
 
 	} // Required function. Called when the plugin is activated (including after reloads)
 
